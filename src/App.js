10fb961b9c58ee1330fdb56fc6afa6726ebb91aa
Ian Raburu <williamraburu@gmail.com>
2023-08-02 23:30:27 +0300

    Wrap searchBar component in ProductsProvider

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
 import SearchBar from './Components/SearchBar';
-import ProductsContext from './Context/ProductsContext';
+import ProductsProvider from './Context/ProductsContext';
+
 
 function App() {
 
   return (
-    <ProductsContext.Provider>
+    <ProductsProvider>
       <SearchBar />
-    </ProductsContext.Provider>
+    </ProductsProvider>
   )
 
 }
