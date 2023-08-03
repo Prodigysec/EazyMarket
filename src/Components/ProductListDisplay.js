0a1fa75740df300856a6cde05ec871242b801820
gilbert2003-beep <gilbert.bageni@student.moringaschool.com>
2023-08-03 14:00:03 +0300

    added boothstrap syling for search bar and made interactive through the whole code

--- src/Components/ProductListDisplay.js
+++ src/Components/ProductListDisplay.js
@@ -1,46 +1,77 @@
+// First, we need some special tools to build our web page.
+// The 'React' tool is like magic for making things on the screen change when we interact with them.
 import React, { useState } from 'react';
+
+// Then, we get some special design elements to create our web page layout.
+// We import 'Row' and 'Col' from the 'react-bootstrap' library.
 import { Row, Col } from 'react-bootstrap';
+
+// Next, we need two custom components that we made in other files.
+// These components help us show product cards and product details.
 import ProductCard from './ProductCard';
 import ProductDetails from './ProductDetails';
 
-const ProductListDisplay = ({ products, selectedCategory }) => {
-  // Define a state variable called selectedProductId and a function to update it called setSelectedProductId
+// Now, we want to make something called 'ProductListDisplay'.
+// It's like a special box that shows a list of products on our web page.
+// It takes some special messages (props) from the parent to know what products to show, which category is selected, and what the user is searching for.
+function ProductListDisplay({ products, selectedCategory, searchQuery }) {
+
+  // Inside our 'ProductListDisplay', we need to remember the ID of the product the user clicks on.
+  // So, we use a special tool called 'useState', which gives us a special box (state) to store the selected product ID.
   const [selectedProductId, setSelectedProductId] = useState(null);
 
-  // Define a function called handleProductClick that takes a productId as an argument
+  // We also need a magic spell (function) called 'handleProductClick'.
+  // When the user clicks on a product, this spell will take the product's ID and put it into our special box using the 'setSelectedProductId' tool.
   const handleProductClick = (productId) => {
-    // When the handleProductClick function is called, update the selectedProductId state with the selected productId
     setSelectedProductId(productId);
   };
 
-  // Filter the products based on the selected category, if any
+  // Now, we want to find all the unique categories from our list of products and put them in an array.
+  // We use the magic tool called 'Set' to help us with this.
+  const categories = [...new Set(products.map((product) => product.category))];
+
+  // Next, we want to filter our products based on the selected category.
+  // If a category is selected, we keep only the products that belong to that category; otherwise, we keep all the products.
   const filteredProducts = selectedCategory
     ? products.filter((product) => product.category === selectedCategory)
     : products;
 
-  // Find the selected product object from the filtered products based on the selectedProductId state
+  // Now, we want to search for products if the user typed something in the search box.
+  // We look for products whose title includes the search query, and we do this in a case-insensitive way to make it easy for the user.
+  const searchResults = searchQuery
+    ? filteredProducts.filter((product) =>
+        product.title.toLowerCase().includes(searchQuery.toLowerCase())
+      )
+    : filteredProducts;
+
+  // After all this magic, we want to find the product that matches the selected product ID.
+  // This will be the product that the user clicked on, and we'll show its details on the screen.
   const selectedProduct = filteredProducts.find((product) => product.id === selectedProductId);
 
-  // Return the JSX (React elements) to be rendered on the screen
+  // Finally, we return the things we want to show on the screen.
   return (
     <div>
-      {/* If a product is selected (selectedProduct is truthy), render the ProductDetails component with the selected product */}
+      {/* If we found a selected product (meaning the user clicked on a product), */}
+      {/* we show the 'ProductDetails' component to display the product's details and a button to go back to the product list. */}
       {selectedProduct && (
         <ProductDetails product={selectedProduct} onBackToList={() => setSelectedProductId(null)} />
       )}
 
-      {/* Render a row of product cards using the Bootstrap grid system */}
-      <Row xs={1} md={2} lg={4}> {/* Change the lg value to 4 to display four cards in a row on large screens */}
-        {filteredProducts.map((product) => (
+      {/* We want to show the product cards in a row, and we use 'Row' and 'Col' for that. */}
+      {/* Each 'Col' will contain a 'ProductCard' component to show a product's information. */}
+      {/* We loop through the 'searchResults' array to show all the products that match the search or selected category. */}
+      <Row xs={1} md={2} lg={4}>
+        {searchResults.map((product) => (
           <Col key={product.id} className="mb-4">
-            {/* Render the ProductCard component, passing it the product object and the handleProductClick function */}
+            {/* We pass each product to the 'ProductCard' component to show its details. */}
+            {/* We also add a magic spell (event handler) so that when the user clicks on a product, we remember its ID. */}
             <ProductCard product={product} onClick={() => handleProductClick(product.id)} />
           </Col>
         ))}
       </Row>
     </div>
   );
-};
+}
 
-// Export the ProductListDisplay component so that other parts of the app can use it
+// After all this magic, we want to share our special 'ProductListDisplay' box with others, so they can use it too!
 export default ProductListDisplay;
--- src/Context/ProductsContext.js
+++ src/Context/ProductsContext.js
@@ -1,28 +1,27 @@
-import { createContext, useEffect, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
-export const ProductsContext = createContext()
+export const ProductsContext = createContext();
 
-const ProductsProvider = ({ children }) => {
-    const [products, setProducts] = useState([]);
+export const ProductsProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        fetch('https://fakestoreapi.com/products')
-            .then(res => res.json())
-            .then(data => setProducts(data))
-    })
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(data => setProducts(data))
+      .catch(error => console.error('Failed to fetch products:', error));
+  }, []);
 
-    const searchProduct = (productName) => {
-        const filteredProducts = products.filter((product) =>
-            product.title.toLowerCase().includes(productName.toLowerCase())
-        );
-        return filteredProducts;
-    };
-
-    return (
-        <ProductsContext.Provider value={{ products, searchProduct }}>
-            {children}
-        </ProductsContext.Provider>
+  const searchProduct = (productName) => {
+    const filteredProducts = products.filter((product) =>
+      product.title.toLowerCase().includes(productName.toLowerCase())
     );
-}
+    return filteredProducts;
+  };
 
-export default ProductsProvider;
+  return (
+    <ProductsContext.Provider value={{ products, searchProduct }}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
