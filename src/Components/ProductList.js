6f4b21adc7443d3e2fbd213b66153b7770dd06e6
gilbert2003-beep <gilbert.bageni@student.moringaschool.com>
2023-08-03 17:17:49 +0300

    Resolve conflicts in ProductList.js and update filter.js

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -4,10 +4,15 @@ import ProductCard from './ProductCard';
 import Cart from './Cart';
 import CategoryFilter from './CategoryFilter';
 import ProductListDisplay from './ProductListDisplay';
+import Wishlist from './Wishlist';
+import Favorites from './Favorites';
 
-const ProductList = ({ products }) => {
-  // Define a state variable called selectedCategory and a function to update it called setSelectedCategory
+// Define the main component 'ProductList' and pass it two props: 'products' and 'searchQuery'
+function ProductList({ products, searchQuery }) {
+  // Define some state variables using the 'useState' hook
   const [selectedCategory, setSelectedCategory] = useState('');
+  const [wishlist, setWishlist] = useState([]); // Assuming you need these state variables.
+  const [favorites, setFavorites] = useState([]); // You may need to initialize them accordingly.
 
   // Create an array of unique categories by extracting them from the products array
   const categories = [...new Set(products.map((product) => product.category))];
@@ -18,36 +23,56 @@ const ProductList = ({ products }) => {
     setSelectedCategory(category);
   };
 
+  // Implement functions to handle adding/removing products from the wishlist and favorites lists
+  const addToWishlist = (product) => {
+    setWishlist((prevWishlist) => [...prevWishlist, product]);
+  };
+
+  const removeFromWishlist = (productId) => {
+    setWishlist((prevWishlist) => prevWishlist.filter((product) => product.id !== productId));
+  };
+
+  const addToFavorites = (product) => {
+    setFavorites((prevFavorites) => [...prevFavorites, product]);
+  };
+
+  const removeFromFavorites = (productId) => {
+    setFavorites((prevFavorites) => prevFavorites.filter((product) => product.id !== productId));
+  };
+
   // Return the JSX (React elements) to be rendered on the screen
   return (
     <Container>
       <Row>
         <Col md={8}>
-          {/* Render the CategoryFilter component, passing it the categories, selectedCategory, and handleSelectCategory props */}
-          <Row>
-            <Col>
-              <CategoryFilter
-                categories={categories}
-                selectedCategory={selectedCategory}
-                onSelectCategory={handleSelectCategory}
-              />
-            </Col>
-          </Row>
-          {/* Render the ProductListDisplay component, passing it the products and selectedCategory props */}
-          <Row>
-            <Col>
-              <ProductListDisplay products={products} selectedCategory={selectedCategory} />
-            </Col>
-          </Row>
+          {/* Render the 'CategoryFilter' component */}
+          <CategoryFilter
+            categories={categories}
+            selectedCategory={selectedCategory}
+            onSelectCategory={handleSelectCategory}
+          />
+          {/* Render the 'ProductListDisplay' component */}
+          <ProductListDisplay
+            products={products}
+            selectedCategory={selectedCategory}
+            searchQuery={searchQuery}
+            onAddToWishlist={addToWishlist}
+            onAddToFavorites={addToFavorites}
+          />
         </Col>
         {/* Render the Cart component in a separate column */}
         <Col md={4}>
+          {/* Render the 'Cart' component */}
           <Cart />
+          {/* Render the 'Wishlist' component */}
+          <Wishlist wishlist={wishlist} onRemoveFromWishlist={removeFromWishlist} />
+          {/* Render the 'Favorites' component */}
+          <Favorites favorites={favorites} onRemoveFromFavorites={removeFromFavorites} />
         </Col>
       </Row>
     </Container>
   );
-};
+}
 
 // Export the ProductList component so that other parts of the app can use it
 export default ProductList;
